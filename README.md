9df13c052680b0b433e7edf55dfc153ab3308e0e
Nick Aversano <nick23hi@gmail.com>
2017-02-26 11:30:02 -0800

    began new site

---
+++ README.md
@@ -0,0 +1,16 @@
+# Personal Site
+
+Built using Jekyll
+
+## Setup
+
+Install ruby. Run:
+```
+gem install jekyll
+```
+
+## Run
+
+```
+jekyll serve
+```
--- js/main.js
+++ js/main.js
@@ -1,29 +1,38 @@
 window.addEventListener('load', function() {
   // spam-free email links (a.mail)
-  var mails = document.getElementsByClassName('mail');
+  var $mails = document.getElementsByClassName('mail');
   var mail;
-  for (var i=0,n=mails.length; i<n; i++) {
-    mail = mails[i];
-    mail.setAttribute('href', mail.getAttribute('href').replace('nospam', 'nick'));
+  for (var i=0,n=$mails.length; i<n; i++) {
+    mail = $mails[i];
+    mail.setAttribute('href', mail.getAttribute('href').replace('nospam', 'mailto:'));
     mail.setAttribute('href', mail.getAttribute('href').replace('+at+', '@'));
   }
-  
-  // mobile menu toggle
-  var toggle = document.getElementById('toggle');
-  var open = false;
-  toggle.addEventListener('click', function() {
-    toggleClass(document.body, 'active', (open = !open));
-  });
 
   // fix external links
-  var pageContent = document.getElementById('page-content');
-  var links = pageContent.getElementsByTagName('a');
+  var $pageContent = document.getElementById('page-content');
+  var links = $pageContent.getElementsByTagName('a');
   for (var i=0,n=links.length; i<n; i++) {
-    if (links[i].href.substr(0, 4) == 'http' && links[i].origin != window.location.origin) {
-      links[i].target = "_blank";
+    if (links[i].href.substr(0, 4) === 'http' && links[i].origin !== window.location.origin) {
+      links[i].target = '_blank';
     }
   }
+
+  // invert color theme
+  var $logo = document.getElementById('logo');
+  var dark = false;
+  if (window.location.pathname === '/') {
+    $logo.addEventListener('click', function(e) {
+      toggleClass(document.body, 'dark-theme', (open = !open));
+      e.preventDefault();
+    });
+  }
+
+  // set current year
+  var $year = document.getElementById('year');
+  $year.innerHTML = new Date().getFullYear();
 });
+
+// helpers
 
 function addClass(el, className) {
   if (el.classList)
